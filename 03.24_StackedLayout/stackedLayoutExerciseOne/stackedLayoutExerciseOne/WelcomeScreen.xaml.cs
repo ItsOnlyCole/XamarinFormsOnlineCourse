5545d71826f17db40b4af430457ad00e6e9ab15a
user171702 <user171702@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2020-05-04 12:21:47 -0400

    Complete 03.24 Exercises one and two

---
+++ 03.24_StackedLayout/stackedLayoutExerciseOne/stackedLayoutExerciseOne/WelcomeScreen.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace stackedLayoutExerciseOne
+{
+    public partial class WelcomeScreen : ContentPage
+    {
+        public WelcomeScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
